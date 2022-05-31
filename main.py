74c11acb86fa8384a61bd1d4fa3c6135aedbd44a
Asif <asifmohammed728@gmail.com>
2022-05-31 10:38:23 +0530

    post

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify, request, render_template
 
+
 app = Flask(__name__)
 
 
-@app.route('/about', methods=['GET'])
+@app.route('/home', methods=['GET'])
 def helloworld():
 	if(request.method == 'GET'):
 		# return jsonify ({'name': 'Jimit',
@@ -15,6 +16,19 @@ def helloworld():
 def nxt_api():
 	if(request.method == 'GET'):
 		return '<h1>Asif <br> Alappuzha</h1>'
+
+
+
+@app.route('/array', methods=['POST'])
+def addition_array():
+    inputdata = request.json
+    sum = 0
+    for number in inputdata['num']:
+        sum = sum + number
+    return str(sum)
+
+
+
 
 
 if __name__ == '__main__':
